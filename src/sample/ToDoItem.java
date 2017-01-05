b2f487d549a1208684c113d9ea1778dd574fd747
Ser-Castic <samsteinberg90@gmail.com>
2017-01-05 16:08:12 -0500

    Desktop App that lets you add/remove/toggle items to/from list

---
+++ src/sample/ToDoItem.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text) {
+        this.text = text;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return text + (isDone ? " (done)" : "");
+    }
+}
